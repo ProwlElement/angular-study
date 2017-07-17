6f65c7e8953b2b200e71c3c71d78cd7b43db3715
Sean Corcoran <corcoransean28@gmail.com>
2017-07-17 17:18:15 +0100

    004 - ngFor loop // course of courses

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,20 @@ import {Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <h2>Courses</h2>
+        <h2>{{getTitle()}}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{course}}
+            </li>
+        </ul>
     `
 })
 
 export class CoursesComponent {
-    
+    title = "List of courses";
+    courses = ["1", "2", "3"];
+
+    getTitle(){
+        return this.title;
+    }
 }
