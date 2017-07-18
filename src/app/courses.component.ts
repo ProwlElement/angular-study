307f310c9e8cac144a3e1965eb040b6a9b5f5605
Sean Corcoran <corcoransean28@gmail.com>
2017-07-18 14:20:31 +0100

    008 - toggle button style using angular property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,22 +2,20 @@ import { CoursesService } from './courses.service';
 import {Component } from '@angular/core';
 
 @Component({
+    // tslint:disable-next-line:component-selector
     selector: 'courses',
     template: `
-        <h2>{{title}}</h2>
-        <ul>
-            <li *ngFor="let course of courses">
-                {{course}}
-            </li>
-        </ul>
+        <button (click)="onSave($event)" [style.backgroundColor]="isActive ? 'blue' : 'green'" type="button" class="btn btn-primary">
+            Save
+        </button>
     `,
 })
 
 export class CoursesComponent {
-    title = "List of courses";
-    courses;
+    isActive = true;
 
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
+    onSave($event) {
+        this.isActive = !this.isActive;
     }
+
 }
