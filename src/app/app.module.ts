d6facae3a64b69ac6b406073e040669576f739de
Sean Corcoran <corcoransean28@gmail.com>
2017-07-18 16:31:38 +0100

    011 - implementing a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,17 @@ import { CourseComponent } from './course/course.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
     CoursesComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -6,22 +6,14 @@ import { Component, NgModule } from '@angular/core';
     // tslint:disable-next-line:component-selector
     selector: 'courses',
     template:  `
-        {{ course.title | uppercase}} <br/>
-        {{ course.rating | number }} <br/>
-        {{ course.students | number:'1.2-4'}} <br/>
-        {{ course.price | currency:'EUR':true }} <br/>
-        {{ course.releaseDate | date:'shortDate' }} <br/>
+        {{ text | summary:30 }}
     `,
 })
 
 export class CoursesComponent {
-    course = {
-        title: 'Prowlmusic event',
-        rating: 9.47,
-        students: 300000,
-        price: 80,
-        releaseDate: new Date(2016, 2, 1)
-    };
+    text = `
+        aaaaasasasasaksakjgkdjgksgkdfgldgfldhgflshdgfslgflsdhgflsdhjgflshgflshdgflsjghdfdlsshhhhhhhhhhhhhhhhhhhhhh
+    `;
 }
 
 
