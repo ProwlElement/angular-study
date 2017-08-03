fb19a701f42fb8992c8482d8000f997118936b38
Sean Corcoran <corcoransean28@gmail.com>
2017-08-03 16:21:20 +0100

    014 - pipe assignment exercise (finished)

--- src/app/title-case.pipe.ts
+++ src/app/title-case.pipe.ts
@@ -3,17 +3,31 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'titleCase'
 })
+
 export class TitleCasePipe implements PipeTransform {
 
   transform(value: string): any {
-    if (!value){
-      return null;
-    }else {
-      let words = value.split(' ');
-      for (var i = 0; i < words.length; i++)
+    // tslint:disable-next-line:curly
+    if (!value) return null;
+    // else
+    const words = value.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      if (i > 0 && this.isPreposition(word)) {
+        words[i] = word.toLowerCase();
+      }else {
+        words[i] = this.toTitleCase(word);
+      }
     }
-
-
+    return words.join(' ');
   }
 
+  private isPreposition(word: string): boolean {
+    const prepositions = ['of', 'the', 'if', 'is'];
+    return prepositions.includes(word.toLowerCase());
+  }
+
+  private toTitleCase(word: string): string {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+  }
 }
